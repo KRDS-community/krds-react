949709c67e062c5fbec38283a8af92152d301e0f
Lukas.J.Han <lukas.j.han@gmail.com>
2024-08-01 21:33:05 +0900

    fix: font setting
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- packages/core/tailwind.config.js
+++ packages/core/tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme';
 import krdsPlugin from '@krds-ui/tailwindcss-plugin';
 
 export default {
   content: ['./lib/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          'Pretendard GOV Variable',
-          'Pretendard GOV',
-          ...defaultTheme.fontFamily.sans,
-        ],
-      },
-    },
-  },
+  theme: {},
   plugins: [krdsPlugin],
 };
