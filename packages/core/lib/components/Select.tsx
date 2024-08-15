99892d74908bac29ec9dc384f140097494b42384
tree <hhyjs2r@gmail.com>
2024-08-15 17:08:28 +0900

    style: change style

--- packages/core/lib/components/Select.tsx
+++ packages/core/lib/components/Select.tsx
@@ -41,14 +41,10 @@ interface SelectItemProps {
   children: React.ReactNode;
 }
 
-const SelectTrigger: React.FC<SelectTriggerProps> = ({
-  onClick,
-  children,
-  isOpen,
-}) => (
+const SelectTrigger = ({ onClick, children, isOpen }: SelectTriggerProps) => (
   <button
     onClick={onClick}
-    className={`w-[220px] py-2 px-4 border ${
+    className={`w-[220px] py-3 px-4 border ${
       isOpen ? 'border-blue-500' : 'border-gray-300'
     } rounded-md text-left bg-white shadow-sm hover:shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 flex justify-between items-center`}
   >
@@ -62,12 +58,11 @@ const SelectTrigger: React.FC<SelectTriggerProps> = ({
     </span>
   </button>
 );
-const SelectValue: React.FC<SelectValueProps> = ({
-  selectedValue,
-  placeholder,
-}) => <div>{selectedValue ? selectedValue : placeholder}</div>;
+const SelectValue = ({ selectedValue, placeholder }: SelectValueProps) => (
+  <div>{selectedValue ? selectedValue : placeholder}</div>
+);
 
-const SelectContent: React.FC<SelectContentProps> = ({ isOpen, children }) => (
+const SelectContent = ({ isOpen, children }: SelectContentProps) => (
   <div
     className={`absolute w-[220px] border border-gray-300 rounded-md bg-white shadow-lg z-10 left-0 mt-2 ${
       isOpen ? 'block' : 'hidden'
@@ -77,31 +72,29 @@ const SelectContent: React.FC<SelectContentProps> = ({ isOpen, children }) => (
   </div>
 );
 
-const SelectGroup: React.FC<SelectGroupProps> = ({ children }) => (
-  <div className="p-2">{children}</div>
+const SelectGroup = ({ children }: SelectGroupProps) => (
+  <div className="p-4">{children}</div>
 );
 
-const SelectLabel: React.FC<SelectLabelProps> = ({ children }) => (
-  <div className="mb-1 font-semibold text-gray-700">{children}</div>
+const SelectLabel = ({ children }: SelectLabelProps) => (
+  <div className="py-2 font-semibold text-gray-700">{children}</div>
 );
-
-const SelectItem: React.FC<SelectItemProps> = ({
+const SelectItem = ({
   value,
   onClick,
   isSelected,
   children,
-}) => (
+}: SelectItemProps) => (
   <button
     onClick={() => onClick(value)}
-    className={`w-full p-2 text-left ${
-      isSelected ? 'text-blue-700 font-semibold' : 'text-gray-900'
-    } hover:bg-blue-100`}
+    className={`w-full p-3 text-left px-6  ${
+      isSelected ? 'text-blue-500 font-semibold' : 'text-gray-900'
+    } hover:bg-blue-100 hover:text-blue-500 hover:rounded-md`}
   >
     {children}
   </button>
 );
-
-const Select: React.FC<SelectProps> = ({ options, placeholder }) => {
+const Select = ({ options, placeholder }: SelectProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedValue, setSelectedValue] = useState<string | null>(null);
 
@@ -135,7 +128,6 @@ const Select: React.FC<SelectProps> = ({ options, placeholder }) => {
   );
 };
 
-// 예시로 사용할 데이터
 const options = [
   { value: 'option1', label: '옵션1' },
   { value: 'option2', label: '옵션2' },
@@ -143,7 +135,7 @@ const options = [
   { value: 'option4', label: '옵션4' },
 ];
 
-const SelectDemo: React.FC = () => {
+const SelectDemo = () => {
   return (
     <div className="p-4">
       <Select options={options} placeholder="플레이스홀더" />
