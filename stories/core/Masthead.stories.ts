bd8af604acc45a9ea4d5ccf764633dfa6768514b
Park Jeongyong <kladess@gmail.com>
2024-12-26 15:49:19 +0900

    feat: add masthead component (#94)
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>
    Co-authored-by: Lukas.J.Han <lukas.j.han@gmail.com>

---
+++ stories/core/Masthead.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { Masthead } from "../../packages/core/lib";
+
+const meta = {
+  title: "Components/Masthead",
+  component: Masthead,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Masthead>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
