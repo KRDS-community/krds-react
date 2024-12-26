12bef2e4bf07c4115809e5de3deb3ccedb795168
Lukas.J.Han <lukas.j.han@gmail.com>
2024-12-26 15:46:43 +0900

    chore: apply formatting
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- stories/core/Masthead.stories.ts
+++ stories/core/Masthead.stories.ts
@@ -1,14 +1,12 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
-import { Masthead } from '../../packages/core/lib';
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { Masthead } from "../../packages/core/lib";
 
 const meta = {
-    title: 'Components/Masthead',
-    component: Masthead,
-    tags: ['autodocs'],
-    
-
-}satisfies Meta<typeof Masthead>;
+  title: "Components/Masthead",
+  component: Masthead,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Masthead>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
