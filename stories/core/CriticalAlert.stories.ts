2578bf09e3a95e00dd2ffd471a7c634962b0e3d4
Jeongyong Park <kladess@gmail.com>
2024-12-27 10:42:35 +0900

    feat: add CriticalAlert component (#52)
    Signed-off-by: Jeongyong Park <kladess@gmail.com>

---
+++ stories/core/CriticalAlert.stories.ts
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { CriticalAlert } from '../../packages/core/lib';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+export default {
+    title: 'Components/CriticalAlert',
+    component: CriticalAlert,
+    tags: ['autodocs'],
+    parameters: {
+        backgrounds: { default: 'dark' },
+        
+        viewport: {
+            defaultViewport: 'desktop',
+            viewport: {
+                viewports: INITIAL_VIEWPORTS,
+                defaultViewport: 'ipad',
+            },
+        },
+    },
+} as Meta<typeof CriticalAlert>;
+
+type Story = StoryObj<typeof CriticalAlert>;
+
+export const Danger: Story = {
+    args: {
+        text: '미세먼지 노출 시 철저한 위생관리로 건강관리에 유의바랍니다.',
+        variant: 'danger',
+        link: 'https://github.com/KRDS-community/krds-react',
+        title: 'krds-react로 이동',
+    },
+};
+
+export const Info: Story = {
+    args: {
+        text: '미세먼지 노출 시 철저한 위생관리로 건강관리에 유의바랍니다.',
+        variant: 'info',
+        link: 'https://github.com/KRDS-community/krds-react',
+        title: 'krds-react로 이동',
+    },
+};
+
+export const Ok: Story = {
+    args: {
+        text: '미세먼지 노출 시 철저한 위생관리로 건강관리에 유의바랍니다.',
+        variant: 'ok',
+        link: 'https://github.com/KRDS-community/krds-react',
+        title: 'krds-react로 이동',
+    },
+};
